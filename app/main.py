30dfccebdef8c06f8b10dbfa888f09ef1101fac0
Piyaphat Jaiboon <fiw.contact.work@gmail.com>
2026-05-03 21:15:26 +0700

    init(FastAPI): Create app folder which is "Entry Point" for FastAPI

---
+++ app/main.py
@@ -0,0 +1,12 @@
+"""
+Filename:   main.py
+Author:     Piyaphat Jaiboon
+Data:       2026-05-03 (03/05/2026)
+Version:    0.1.0
+Description: Entry point for FastAPI  App
+             Used to bind URL paths to function
+"""
+
+from fastapi import FastAPI
+
+app = FastAPI()
